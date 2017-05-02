211a73bf48a53148128a3fc514c0810d80ec51ab
Yaroslav209 <yaroslavsapegin@gmail.com>
2017-05-02 16:46:06 +0300

    Create Class_String.cpp

---
+++ Class_String.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+string A = "WTH"
+string B;
+string C;
+string s;
+cin >> B;
+cin >> C;
+cin >> s;
+bool a = (A == C);
+cout << A + B;
+// сложение строк A и B
+cout << a;
+// сравнение строк A и C
+cout << A.find(s);
+// поиск строки s в A
+return 0;
+}
